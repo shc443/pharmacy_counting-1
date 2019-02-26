bf1cc6970d7cea520428f3863dd0b2e5e94c24c7
shc443 <shc443@nyu.edu>
2019-02-26 04:36:28 -0500

    Update pharmacy_counting.py

--- src/pharmacy_counting.py
+++ src/pharmacy_counting.py
@@ -9,7 +9,11 @@ import os
 
 inputPath = str(sys.argv[1])
 outputPath = str(sys.argv[2])
+
 import pandas as pd 
 df1 = pd.read_csv(inputPath)
 df2 = df1.groupby('drug_name').agg({'drug_cost' : [pd.Series.nunique,'sum']})
-df2.sort_values(by=[('drug_cost','sum'),'drug_name'], ascending = [False, True]).to_csv(outputPath)
+df3 = df2.sort_values(by=[('drug_cost','sum'),'drug_name'], ascending = [False, True])
+df3 = df3['drug_cost'].reset_index()
+df3.columns = ['drug_name','num_prescriber','total_cost'] 
+df3.to_csv(outputPath, index = False)
