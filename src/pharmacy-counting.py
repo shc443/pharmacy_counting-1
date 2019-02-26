8042d170fd16fb379c389a87d5095d9c1251bc7a
shc443 <shc443@nyu.edu>
2019-02-26 03:20:06 -0500

    Create pharmacy-counting.py

---
+++ src/pharmacy-counting.py
@@ -0,0 +1,32 @@
+import csv
+
+#Data Import
+txt = open('de_cc_data.txt');
+dat = txt.read()
+dat = dat.split('\n');
+
+#Data Calc
+pre = {}
+df = {}
+for line in new[1:]:
+    try:
+        if line[0] not in pre[line[3]]:
+            pre[line[3]].append(line[0])
+    except:
+        pre[line[3]] = [line[0]]
+    try:
+        df[line[3]] = [len(pre[line[3]]),df[line[3]][1]+eval(line[4])]
+    except:
+        df[line[3]] = [len(pre[line[3]]),eval(line[4])]
+        
+#Sorting
+LI = []
+for i in df.items():
+    LI.append([i[0],i[1][0],i[1][1]])
+LI.sort(key = lambda x : (-x[2], x[0]))
+LI.insert(0,['drug_name','num_prescriber','total_cost'])
+
+#Saving
+with open("top_cost_drug.txt", "w") as f:
+    writer = csv.writer(f)
+    writer.writerows(LI)
