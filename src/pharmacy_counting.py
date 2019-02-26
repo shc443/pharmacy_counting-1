73500ff8662b6d4f483765d880961431c51d3254
shc443 <shc443@nyu.edu>
2019-02-26 04:13:03 -0500

    Update pharmacy_counting.py

--- src/pharmacy_counting.py
+++ src/pharmacy_counting.py
@@ -11,6 +11,36 @@ inputPath = str(sys.argv[1])
 outputPath = str(sys.argv[2])
 
 #Data Import
+txt = txt = open(inputPath);
+dat = txt.read()
+dat = dat.split('\n');
+
+#Data Calc
+pre = {}
+df = {}
+new = []
+for line in dat:
+    new.append(line.split(','))
+    
+for line in new[1:]:
+    try:
+        if line[0] not in pre[line[3]]:
+            pre[line[3]].append(line[0])
+    except:
+        pre[line[3]] = [line[0]]
+    try:
+        df[line[3]] = [len(pre[line[3]]),df[line[3]][1]+eval(line[4])]
+    except:
+        df[line[3]] = [len(pre[line[3]]),eval(line[4])]
+        
+#Sorting
+LI = []
+for i in df.items():
+    LI.append([i[0],i[1][0],i[1][1]])
+LI.sort(key = lambda x : (-x[2], x[0]))
+LI.insert(0,['drug_name','num_prescriber','total_cost'])
+
+#Data Import
 
 with open(outputPath, "w") as f:
     writer = csv.writer(f)
